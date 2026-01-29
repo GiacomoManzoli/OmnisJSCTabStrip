f45b91bfc55b8bf43bb78633c719735052e88d00
Giacomo Manzoli <giacomo.manzoli@gmail.com>
2026-01-29 10:18:20 +0100

    Updated icon for add

--- src/TabStrip.ts
+++ src/TabStrip.ts
@@ -233,7 +233,23 @@ export class TabStrip {
         li.classList.add("action-add")
 
         const a = document.createElement("a")
-        a.innerHTML = "+" //+ "&#10006;" + "&#x2715;"
+        // a.innerHTML = "+" //+ "&#10006;" + "&#x2715;"
+        a.innerHTML =`
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+        >
+            <line x1="12" y1="5" x2="12" y2="19"></line>
+            <line x1="5" y1="12" x2="19" y2="12"></line>
+        </svg>
+        `
 
         li.append(a)
 
@@ -282,6 +298,8 @@ export class TabStrip {
                     labelSpan = this.createAddLabel(li)
                 }
                 labelSpan.innerText = this.addTabText
+            } else {
+                this.removeAddLabel(li)
             }
         }
     }
